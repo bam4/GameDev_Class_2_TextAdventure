1e7ccb9ef485b229c014a26704cc30ffca1cfa2d
Brett Moody <brettmoody@me.com>
2016-07-11 16:03:42 -0400

    These are the projects I did in class.

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+//This line will let your code to the text.
+
+	public class HelloWorld : MonoBehaviour {
+
+	public Text myTextObject;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hello world");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("Bonjour Monde");
+
+		myTextObject.text = "Hola Mundo";
+	}
+}
---
+++ Assets/Scripts/TapGame.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TapGame : MonoBehaviour {
+
+	public Text myTextObject2;
+
+	int currentPoints = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Give player one point if they press space.
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			currentPoints += 1;
+			myTextObject2.text = "Current Score: " + currentPoints.ToString();
+		}
+
+		// Provide win condition.
+		if (currentPoints >= 15) {
+			myTextObject2.text = "Congratulations, you win!";
+
+			if (Input.GetKeyDown ((KeyCode.y)){
+				currentPoints += 500;
+		}
+	}
+}
